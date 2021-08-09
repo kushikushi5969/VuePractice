4100cd7b17ead8c8ee602fa8ebfd27cd9ad68650
kushikushi5969 <yuya.kushida1221@gmail.com>
2021-08-09 22:40:59 +0900

    スロット設定

--- udemy-vue.js/src/App.vue
+++ udemy-vue.js/src/App.vue
@@ -1,7 +1,18 @@
 <template>
   <div>
-    <LikeHeader></LikeHeader>
-    <LikeNumber></LikeNumber>
+    <LikeHeader header-text="hello">
+      <h2>みなさん</h2>
+      <template v-slot:title>
+        <h1>こんにちは</h1>
+      </template>
+      <h3>初めまして</h3>
+      <p>よろしくお願いします</p>
+      <template v-slot:number>
+        <h2>{{number}}</h2>
+      </template>
+    </LikeHeader>
+    <LikeNumber :total-number="number" @my-click="number = $event"></LikeNumber>
+    <LikeNumber :total-number="number"></LikeNumber>
   </div>
 </template>
 
@@ -9,6 +20,11 @@
 import LikeHeader from "./components/LikeHeader";
 
 export default {
+  data() {
+    return {
+      number: 14
+    }
+  },
   components: {
     LikeHeader: LikeHeader
   }
@@ -18,5 +34,8 @@ export default {
 <style scoped>
   div {
     border: blue 1px solid;
+  }
+  h1 {
+    color: red;
   }
 </style>
--- udemy-vue.js/src/components/LikeHeader.vue
+++ udemy-vue.js/src/components/LikeHeader.vue
@@ -1,3 +1,16 @@
 <template>
-  <h1>いいね</h1>
+  <div>
+    <slot name="title"></slot>
+    <slot></slot>
+    <hr>
+    <p>いいねの数</p>
+    <slot name="number"></slot>
+    <p>{{headerText}}</p>
+  </div>
 </template>
+
+<script>
+export default {
+  props: ['headerText']
+}
+</script>
