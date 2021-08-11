f7a79f0ac90543d6b082739ff6fa828dc895027d
kushikushi5969 <yuya.kushida1221@gmail.com>
2021-08-11 21:49:23 +0900

    セレクトボックス

--- udemy-vue.js/src/App.vue
+++ udemy-vue.js/src/App.vue
@@ -10,7 +10,7 @@
     <keep-alive>
       <component :is="currentComponent"></component>
     </keep-alive>
-    <div>
+    <div style="padding: 10rem">
       <h2>イベントのフォーム</h2>
       <label for="title">タイトル</label>
       <input
@@ -87,6 +87,15 @@
         v-model="eventData.price"
       >
       <label for="paid">有料</label>
+
+      <p>開催場所</p>
+      <select v-model="eventData.location" multiple>
+        <option
+          v-for="location in locations"
+          :key="location"
+        >{{ location }}</option>
+      </select>
+      <p>{{ eventData.location }}</p>
     </div>
   </div>
 </template>
@@ -102,13 +111,15 @@ export default {
     return {
       number: 14,
       currentComponent: "Home",
+      locations: ["東京", "大阪", "茨城"],
       eventData: {
         title: "",
         maxNumber: 0,
         detail: "",
         isPrivate: false,
         target: [],
-        price: "無料"
+        price: "無料",
+        location: "東京"
       }
     }
   },
